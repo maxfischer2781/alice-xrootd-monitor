1e3bd41bcdeb8859e7b43aee6c5e501808dd5201
Max Fischer <maxfischer2781@gmail.com>
2017-01-11 15:40:02 +0100

    added logging

--- xrdmonitor.py
+++ xrdmonitor.py
@@ -1,6 +1,7 @@
 from __future__ import division, absolute_import, print_function
 import argparse
 import os
+import logging
 
 from xrdmon import core
 from xrdmon.backend import filepath
@@ -16,10 +17,23 @@ CLI.add_argument(
     help='path to write logs to',
     default='/tmp/%s.log' % (os.path.splitext(os.path.basename(__file__))[0])
 )
+CLI.add_argument(
+    '--log-level',
+    help='Log level to use',
+    default='WARNING'
+)
 
 if __name__ == '__main__':
     options = CLI.parse_args()
-    print('port', options.report_port, 'path', options.file_path)
+    logging.basicConfig()
+    log_level = options.pop('log_level')
+    try:
+        log_level = int(log_level)
+    except ValueError:
+        log_level = getattr(logging, log_level)
+    APP_LOGGER = logging.getLogger(os.path.basename('xrdmon'))
+    APP_LOGGER.setLevel(log_level)
+    APP_LOGGER.info('report_port=%s, file_path=%s', options.report_port, options.file_path)
     main = core.Core(
         report_port=options.report_port,
         backends=[filepath.FileBackend(file_path=options.file_path)]

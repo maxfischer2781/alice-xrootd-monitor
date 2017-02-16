b6e826e78211669a7371287d75f2447c37f92ed8
Max Fischer <maxfischer2781@gmail.com>
2017-02-16 17:21:44 +0100

    removed debug code from debug element

--- xrdmonlib/backend/logger.py
+++ xrdmonlib/backend/logger.py
@@ -5,9 +5,7 @@ import chainlet
 
 
 @chainlet.FunctionLink.linklet
-def debug(value, destination=sys.stderr):
+def debug(value, fmt='%r', destination=sys.stderr):
     """Print the value to `destination` and pass it on unchanged"""
-    print()
-    print('Fooo!!!!', value, file=destination)
-    print()
+    print(fmt % value, file=destination)
     return value
